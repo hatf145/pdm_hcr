0b4f75ccf56516299790b04ac0c44c430d756972
Hector Toscano <hectos145@hotmail.com>
2018-02-24 11:49:10 -0600

    Floating Labels | Ripple Effect Buttons | Register Lessor Activity

--- app/src/main/java/iteso/com/rentstudio/Activity_Register_Lessor.java
+++ app/src/main/java/iteso/com/rentstudio/Activity_Register_Lessor.java
@@ -2,13 +2,12 @@ package iteso.com.rentstudio;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.Spinner;
 
-public class Activity_Register_User extends AppCompatActivity {
+public class Activity_Register_Lessor extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity__register__user);
+        setContentView(R.layout.activity__register__lessor);
     }
 }
