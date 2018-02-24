a915be55a1a79b765a42e6496c53e334b01b21ce
Hector Toscano <hectos145@hotmail.com>
2018-02-23 21:02:47 -0600

    Transición LogIn -> Registro (Simple Click)

--- app/src/main/java/iteso/com/rentstudio/Activity_LogIn.java
+++ app/src/main/java/iteso/com/rentstudio/Activity_LogIn.java
@@ -1,7 +1,9 @@
 package iteso.com.rentstudio;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Activity_LogIn extends AppCompatActivity {
 
@@ -9,5 +11,14 @@ public class Activity_LogIn extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity__log_in);
+    }
+
+    public void onClick(View v){
+        switch (v.getId()){
+            case R.id.activity_login_registerButton:
+                Intent registerIntent = new Intent(Activity_LogIn.this,
+                    Activity_Register_User.class);
+                startActivity(registerIntent);
+        }
     }
 }
