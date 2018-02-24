99a57b6f2d545f7043239ded5a07779b01e096c5
Hector Toscano <hectos145@hotmail.com>
2018-02-23 18:27:21 -0600

    First Commit / Log In Activity

---
+++ app/src/main/java/iteso/com/rentstudio/Activity_LogIn.java
@@ -0,0 +1,13 @@
+package iteso.com.rentstudio;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Activity_LogIn extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity__log_in);
+    }
+}
