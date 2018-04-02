bec0dd6f4a966f35d5b2dc9e0efebc1b557d7f72
RobertoCtz <robertocortez095@gmail.com>
2018-04-01 20:27:46 -0500

    Credit/paypal activity added

---
+++ app/src/main/java/iteso/com/rentstudio/Activity_Payment_Method.java
@@ -0,0 +1,13 @@
+package iteso.com.rentstudio;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Activity_Payment_Method extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity__payment__method);
+    }
+}
