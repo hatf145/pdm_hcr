87456d40609a5e9abf641be535e573aa60b718ee
RobertoCtz <robertocortez095@gmail.com>
2018-04-04 23:42:37 -0500

    Settings activity added

---
+++ app/src/main/java/iteso/com/rentstudio/Activity_Settings.java
@@ -0,0 +1,13 @@
+package iteso.com.rentstudio;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Activity_Settings extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity__settings);
+    }
+}
