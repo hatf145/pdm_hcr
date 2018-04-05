670b19cf66f4eef95c7d811c997223efe703395f
cgaribay-pc\cgaribay <catziri@gmail.com>
2018-04-05 07:32:48 -0500

    Ädded missing project file '

---
+++ app/build/generated/source/buildConfig/debug/iteso/com/rentstudio/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package iteso.com.rentstudio;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "iteso.com.rentstudio";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
